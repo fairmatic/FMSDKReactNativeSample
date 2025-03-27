36cd922f781017d3873d1a013efe13303ff8e5ec
Sagar Dagdu <shags032@gmail.com>
2025-03-27 15:52:38 +0530

    Refactor App component to streamline SDK setup and event handling

--- App.tsx
+++ App.tsx
@@ -1,95 +1,23 @@
 import * as React from 'react';
 
-import { StyleSheet, View, Button, Image, Text } from 'react-native';
+import { StyleSheet, View, Button, Text } from 'react-native';
 import FairmaticSDK, {
-  FairmaticCallbackEventKind,
   type DriverAttributes,
   type FairmaticConfiguration,
   type FairmaticOperationResult,
-  type FairmaticNotificationSettings,
-  type FairmaticNotificationConfig,
-  type FairmaticCallbackEvent,
-  FairmaticEventType,
+  type FairmaticTripNotification,
 } from 'react-native-fairmatic-sdk';
-
-const handleEvent = (event: FairmaticCallbackEvent): void => {
-  console.log('Callback received from Fairmatic SDK in App.js:');
-  // Handle different actions based on the event type
-  switch (event.kind) {
-    case FairmaticCallbackEventKind.ON_DRIVE_START:
-      console.log('Converted Drive Start Data in App.js:', event.event);
-      break;
-    case FairmaticCallbackEventKind.ON_DRIVE_END:
-      console.log('Converted Drive End Data in App.js:', event.event);
-      break;
-    case FairmaticCallbackEventKind.ON_DRIVE_RESUME:
-      console.log('Converted Drive Resume Data in App.js:', event.event);
-      break;
-    case FairmaticCallbackEventKind.ON_DRIVE_ANALYZED:
-      console.log('Converted Drive Analyzed Data in App.js:', event.event);
-      break;
-    case FairmaticCallbackEventKind.ON_ACCIDENT:
-      console.log('Converted Accident Data in App.js:', event.event);
-      break;
-    case FairmaticCallbackEventKind.ON_POTENTIAL_ACCIDENT:
-      console.log('Converted Potential Accident Data in App.js:', event.event);
-      break;
-    case FairmaticCallbackEventKind.ON_SETTINGS_CHANGED:
-      console.log('Converted OnSettingeChanged Data in App.js:', event.event);
-      break;
-    // Handle other event types as needed
-    default:
-      console.log('Unknown event type:', event.kind);
-      break;
-  }
-};
-
-interface ResolvedAssetSource {
-  uri: string;
-}
-
-async function loadImage() {
-  // try {
-  //   const imageUri = (
-  //     Image.resolveAssetSource(
-  //       require('./assets/alarm.png')
-  //     ) as ResolvedAssetSource
-  //   ).uri;
-  //   return imageUri;
-  // } catch (error) {
-  //   console.error('Error loading image from assets:', error);
-  //   throw error;
-  // }
-}
-
-async function createNotifications(): Promise<FairmaticNotificationSettings> {
-  const imageUri = await loadImage();
-  console.log('Base64 Image:', imageUri);
-  const maybeInDriveNotificationConfig: FairmaticNotificationConfig = {
-    title: 'Fairmatic MaybeInDrive',
-    description: 'State is Maybe In Drive',
-    path: imageUri,
-  };
-  const inDriveNotificationConfig: FairmaticNotificationConfig = {
-    title: 'Fairmatic InDrive',
-    description: 'State is In Drive',
-    path: imageUri,
-  };
-  return {
-    maybeInDriveNotificationConfig: maybeInDriveNotificationConfig,
-    inDriveNotificationConfig: inDriveNotificationConfig,
-  };
-}
 
 export default function App() {
   React.useEffect(() => {}, []);
 
-  const handleGetEventTypes = (): void => {
-    FairmaticSDK.getEventSupportForDevice().then(
-      (map: Map<FairmaticEventType, Boolean>) => {
-        console.log('Map : ', map);
-      }
-    );
+  const handleUtility = (): void => {
+    handleGetSDKBuildVersion();
+  };
+  const handleGetSDKBuildVersion = (): void => {
+    FairmaticSDK.getBuildVersion().then((version) => {
+      console.log('Build Version : ', version);
+    });
   };
 
   const handleGetFairmaticSettings = (): void => {
@@ -98,88 +26,94 @@ export default function App() {
     });
   };
 
+  const handleWipeOut = (): void => {
+    console.log('Wiping out Fairmatic SDK');
+    FairmaticSDK.wipeOut()
+      .then((result) => {
+        if (result.isSuccess) console.log('wipeOut successful');
+        else console.log('wipeOut failed because of ', result.errorMessage);
+      })
+      .catch((error) => {
+        console.error('error wiping out', error);
+      });
+  };
+
   const handleSetup = (): void => {
     const driverAttributes: DriverAttributes = {
-      name: 'John Doe',
+      firstName: 'John',
+      lastName: 'Doe',
       email: 'john.doe@example.com',
       phoneNumber: '123-456-7890',
     };
 
-    //create three FmNotifications objects
-    createNotifications().then((notifs) => {
-      const fairmaticConfiguration: FairmaticConfiguration = {
-        sdkKey: 'your_sdk_key_here',
-        driverId: 'driverId',
-        driverAttributes: driverAttributes,
-        notifications: notifs,
-      };
+    const fairmaticNotification: FairmaticTripNotification = {
+      title: 'Fairmatic Trip',
+      content: 'Monitoring a trip',
+      iconResourceName: 'fm_car',
+    };
 
-      FairmaticSDK.setup(fairmaticConfiguration)
-        .then((result: FairmaticOperationResult) => {
-          console.log('response: ', result);
-          if (result.isSuccess) console.log('Setup Succesful');
-          else console.log('Setup Failed because of ', result.errorMessage);
+    const fairmaticConfiguration: FairmaticConfiguration = {
+      sdkKey: '',
+      driverId: 'driverId',
+      driverAttributes: driverAttributes,
+      notification: fairmaticNotification,
+    };
 
-         FairmaticSDK.getBuildVersion().then((version) => {
-            console.log('Build Version: ', version);
-          });
-        })
-        .catch((error: any) => {
-          console.error('Error setting up Fairmatic SDK:', error);
-        });
-
-      // Register the callbacks
-      FairmaticSDK.registerFairmaticCallbackEventListener(handleEvent);
-    });
+    FairmaticSDK.setup(fairmaticConfiguration)
+      .then((result: FairmaticOperationResult) => {
+        console.log('response: ', result);
+        if (result.isSuccess) console.log('Setup Succesful');
+        else console.log('Setup Failed because of ', result.errorMessage);
+      })
+      .catch((error: any) => {
+        console.error('Error setting up Fairmatic SDK:', error);
+      });
   };
 
   const handleStartPeriod1 = (): void => {
-    FairmaticSDK.startDriveWithPeriod1()
-      .then(() => {
-        console.log('Drive started successfully');
-        // Handle success if needed
+    const trackingId: string = 'p1-tracking-id';
+    FairmaticSDK.startDriveWithPeriod1(trackingId)
+      .then((result: FairmaticOperationResult) => {
+        if (result.isSuccess) console.log('P1 drive started successfully');
+        else console.log('Drive failed because of ', result.errorMessage);
       })
       .catch((error: any) => {
         console.error('Error starting P1 drive:', error);
-        // Handle error if needed
       });
   };
 
   const handleStartPeriod2 = (): void => {
-    const trackingId: string = 'p2-tracking-id'; // Pass trackingId
+    const trackingId: string = 'p2-tracking-id';
     FairmaticSDK.startDriveWithPeriod2(trackingId)
-      .then(() => {
-        console.log('Drive started successfully');
-        // Handle success if needed
+      .then((result: FairmaticOperationResult) => {
+        if (result.isSuccess) console.log('P2 drive started successfully');
+        else console.log('Drive failed because of ', result.errorMessage);
       })
       .catch((error: any) => {
         console.error('Error starting P2 drive:', error);
-        // Handle error if needed
       });
   };
 
   const handleStartPeriod3 = (): void => {
-    const trackingId: string = 'p3-tracking-id'; // Pass trackingId
+    const trackingId: string = 'p3-tracking-id';
     FairmaticSDK.startDriveWithPeriod3(trackingId)
-      .then(() => {
-        console.log('Drive started successfully');
-        // Handle success if needed
+      .then((result: FairmaticOperationResult) => {
+        if (result.isSuccess) console.log('P3 drive started successfully');
+        else console.log('Drive failed because of ', result.errorMessage);
       })
       .catch((error: any) => {
         console.error('Error starting P3 drive:', error);
-        // Handle error if needed
       });
   };
 
   const handleStopPeriod = (): void => {
     FairmaticSDK.stopPeriod()
-      .then(() => {
-        console.log('Period drive stopped successfully');
-        // Handle success if needed
+      .then((result: FairmaticOperationResult) => {
+        if (result.isSuccess) console.log('Period stopped successfully');
+        else console.log('Period stop failed because of ', result.errorMessage);
       })
       .catch((error: any) => {
         console.error('Error stopping period:', error);
-        // Handle error if needed
       });
   };
 
@@ -187,11 +121,9 @@ export default function App() {
     FairmaticSDK.teardown()
       .then(() => {
         console.log('Teardown successful');
-        // Handle success if needed
       })
       .catch((error: any) => {
         console.error('Error tearing down Fairmatic SDK:', error);
-        // Handle error if needed
       });
   };
 
@@ -217,10 +149,13 @@ export default function App() {
         <Button title="Teardown" onPress={handleTeardown} />
       </View>
       <View style={styles.buttonContainer}>
+        <Button title="Wipe Out" onPress={handleWipeOut} />
+      </View>
+      <View style={styles.buttonContainer}>
         <Button title="Get Settings" onPress={handleGetFairmaticSettings} />
       </View>
       <View style={styles.buttonContainer}>
-        <Button title="Utility Button" onPress={handleGetEventTypes} />
+        <Button title="Utility Button" onPress={handleUtility} />
       </View>
     </View>
   );
